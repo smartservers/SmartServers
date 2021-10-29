7eba111b3ecc24b9c8de1b37e12f877971fadf18
TwiDev <twidev5@gmail.com>
2021-10-29 22:57:07 +0200

    update

--- common/src/main/java/net/smartservers/common/SmartCommon.java
+++ common/src/main/java/net/smartservers/common/SmartCommon.java
@@ -1,4 +1,7 @@
 package net.smartservers.common;
 
 public class SmartCommon {
+
+
+
 }
---
+++ connector/src/main/java/net/smartservers/connector/SmartConnector.java
@@ -0,0 +1,9 @@
+package net.smartservers.connector;
+
+public class SmartConnector {
+    
+    public static final String NAME = "SmartServices";
+    public static final String GIT_VERSION = "DEV"; // A fallback for running in IDEs
+    public static final String VERSION = "DEV"; // A fallback for running in IDEs
+
+}
