43cb6af559e64a2776417ab0285f3f04eb15f5b7
TwiDev <twidev5@gmail.com>
2021-10-30 14:56:38 +0200

    update maven (add lombok)

---
+++ common/src/main/java/net/smartservers/common/SmartPlatformType.java
@@ -0,0 +1,7 @@
+package net.smartservers.common;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class SmartPlatformType {
+}
--- connector/src/main/java/net/smartservers/connector/SmartConnector.java
+++ connector/src/main/java/net/smartservers/connector/SmartConnector.java
@@ -6,4 +6,11 @@ public class SmartConnector {
     public static final String GIT_VERSION = "DEV"; // A fallback for running in IDEs
     public static final String VERSION = "DEV"; // A fallback for running in IDEs
 
+    private static SmartConnector instance;
+
+
+
+    public static SmartConnector getInstance() {
+        return instance;
+    }
 }
