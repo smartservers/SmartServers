83d685e08bc21a1a590976274ef19be303b2db18
TwiDev <twidev5@gmail.com>
2021-10-30 15:46:49 +0200

    update connector

--- connector/src/main/java/net/smartservers/connector/SmartConnector.java
+++ connector/src/main/java/net/smartservers/connector/SmartConnector.java
@@ -1,22 +1,26 @@
 package net.smartservers.connector;
 
+import lombok.Getter;
 import net.smartservers.common.SmartPlatformType;
 
 public class SmartConnector {
-    
+
+    @Getter
     public static final String NAME = "SmartServices";
+
+    @Getter
     public static final String GIT_VERSION = "DEV"; // A fallback for running in IDEs
+
+    @Getter
     public static final String VERSION = "DEV"; // A fallback for running in IDEs
 
+    @Getter
     private static SmartConnector instance;
 
+    @Getter
     private final SmartPlatformType platformType;
 
     public SmartConnector(SmartPlatformType platformType) {
         this.platformType = platformType;
-    }
-
-    public static SmartConnector getInstance() {
-        return instance;
     }
 }
