a79daeb1b895573e5e599ec2964c290902fbbc1e
TwiDev <twidev5@gmail.com>
2021-12-08 14:39:06 +0100

    update maven project

---
+++ platforms/spigot/src/main/java/eu/snk/spigot/CorePlugin.java
@@ -0,0 +1,11 @@
+package eu.snk.spigot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CorePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
